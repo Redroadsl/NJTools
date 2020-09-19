c74e42b8ddc73d397bf19143ae93092ecc666b84
Redroadsl <50506568+Redroadsl@users.noreply.github.com>
2020-09-19 19:11:33 +0800

    Win_con update
    None

---
+++ wincon.py
@@ -0,0 +1,16 @@
+import ctypes
+import os
+#Create object Screen
+class Screen(object):
+    '''Screen info'''
+    def __init__(self, surface):
+        self.x = ctypes.windll.user32.GetSystemMetrics(0)
+        self.y = ctypes.windll.user32.GetSystemMetrics(1)
+        self.size = (self.x, self.y)
+        self.center = (self.x//2, self.y//2)
+    def Max(self):
+        os.environ['SDL_VIDEO_WINDOW_POS']="%d,%d" %(0,0)
+        surface=pygame.display.set_mode(self.size, 0, 32)
+    def Min(self):
+        os.environ['SDL_VIDEO_WINDOW_POS']="%d,%d" %(self.center)
+        surface=pygame.display.set_mode(self.center, 0, 32)
